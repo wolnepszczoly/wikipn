4682df54937ca7994a1b6e20e49f1416fa6cca19
Konrad Szymański <ko.szymanski@gmail.com>
2015-10-30 11:14:12 +0100

    Wiki first setup

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 Ta strona jest poświęcona ogólnie rozumianemu pszczelarstwu ze szczególnym naciskiem na zagadnienia pszczelarstwa naturalnego. Przyświeca jej idea promowana przez stowarzyszenie [Wolne Pszczoły](http://wolnepszczoly.org) zdrowych i szczęśliwych pszczół bez trucia ich niepotrzebną chemią, która na dzień dzisiejszy stała się przykrą rzeczywistością większości pasiek. Ma to szeroko pojęte negatywne skutki tak dla samych rodzin pszczelich jak i konsumentów końcowych produktów pszczelich.
 
 Zapraszamy do lektury [Wiki](https://github.com/wolnepszczoly/wikipn/wiki)
+
+
+## Informacje na temat budowania wiki
+
+Końcowa Wiki jest budowana statycznie z github wiki (projekt [gollum](https://github.com/gollum/gollum/wiki)) poprzez nasz własny mechanizm przy użyciu pythonowego skryptu i [MkDocs](http://www.mkdocs.org/).
+
+```
+pip install -r requirements.txt
+python convert.py
+mkdocs build
+```
---
+++ convert.py
@@ -0,0 +1,31 @@
+import os
+
+
+def change_links(text):
+    start = text.find('[[')
+    while start != -1:
+        end = text.find(']]') + 2
+	if end == 1: return text
+        between = text[start + 2:end - 2]
+	between = '[%s](%s)' % (between, between.replace(' ', '-') + '.md')
+        text = ''.join((text[:start], between, text[end:]))
+        index = text.find('[[')
+    return text
+        
+
+def convert(source, dest):
+    for path, dirs, files in os.walk(source):
+        if '.git' in path: continue
+        for filename in [f for f in files if f.endswith('.md')]:
+            new_path = path.replace(source, dest, 1)
+            if not os.path.exists(new_path):
+                os.makedirs(new_path)
+            with open(os.path.join(path, filename), 'r') as sf:
+                with open(os.path.join(new_path, filename), 'w') as df:
+                    data = sf.read()
+                    data = change_links(data)
+                    df.write(data)
+
+
+if __name__ == '__main__':
+    convert('wikipn.wiki', 'docs')
